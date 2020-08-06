bad63e51faa4db444750660d29c5bf3a8e9d84e9
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-06 13:22:13 +0800

    fix options

--- src/puppet-oa.spec.ts
+++ src/puppet-oa.spec.ts
@@ -2,12 +2,16 @@
 
 import test  from 'blue-tape'
 
+import { getOaOptions } from '../tests/fixtures/oa-options'
+
 import { PuppetOA } from './puppet-oa'
 
 class PuppetOATest extends PuppetOA {
 }
 
 test('tbw', async t => {
-  const oa = new PuppetOATest()
+  const oa = new PuppetOATest({
+    ...getOaOptions(),
+  })
   t.ok(oa, 'should be ok')
 })
