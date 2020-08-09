1a8ba1860d5c02fc1f8cf966c9d380b7441d1035
Huan LI (李卓桓) <zixia@zixia.net>
2020-08-09 18:39:53 +0800

    fix flooding get acccess token bug

--- src/official-account/official-account.ts
+++ src/official-account/official-account.ts
@@ -102,7 +102,12 @@ class OfficialAccount extends EventEmitter {
 
     await this.payloadStore.start()
 
-    const stopper = await this.updateAccessToken()
+    const succeed = await this.updateAccessToken()
+    if (!succeed) {
+      throw new Error('updateAccessToken failed!')
+    }
+
+    const stopper = await this.startSyncingAccessToken()
     this.stopperFnList.push(stopper)
 
     await this.webhook.start()
@@ -124,70 +129,89 @@ class OfficialAccount extends EventEmitter {
     await this.payloadStore.stop()
   }
 
+  protected async updateAccessToken (): Promise<boolean> {
+    log.verbose('OfficialAccount', 'updateAccessToken()')
+
+    /**
+     * updated: {
+     *  "access_token":"3...Q",
+     *  "expires_in":7200
+     * }
+     */
+    const ret = await this.simpleUnirest
+      .get<Partial<ErrorPayload> & {
+        access_token : string
+        expires_in   : number
+      }>(`token?grant_type=client_credential&appid=${this.options.appId}&secret=${this.options.appSecret}`)
+
+    log.verbose('OfficialAccount', 'updateAccessToken() %s', JSON.stringify(ret.body))
+
+    if (ret.body.errcode && ret.body.errcode > 0) {
+      // {"errcode":40164,"errmsg":"invalid ip 111.199.187.71 ipv6 ::ffff:111.199.187.71, not in whitelist hint: [H.BDtZFFE-Q7bNKA] rid: 5f283869-46321ea1-07d7260c"}
+      log.warn('OfficialAccount', `updateAccessToken() ${ret.body.errcode}: ${ret.body.errmsg}`)
+
+      if (this.accessTokenPayload) {
+        const expireTimestamp = this.accessTokenPayload.timestamp
+          + (this.accessTokenPayload.expiresIn * 1000)
+
+        if (expireTimestamp > Date.now()) {
+          // expired.
+          log.warn('OfficialAccount', 'updateAccessToken() token expired!')
+          this.accessTokenPayload = undefined
+        }
+      }
+
+      return false
+    }
+
+    this.accessTokenPayload = {
+      expiresIn : ret.body.expires_in,
+      timestamp : Date.now(),
+      token     : ret.body.access_token,
+    }
+
+    log.verbose('OfficialAccount', 'updateAccessToken() synced. New token will expiredIn %s seconds',
+      this.accessTokenPayload.expiresIn,
+    )
+
+    return true
+  }
+
   /**
    *  https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Get_access_token.html
    */
-  protected async updateAccessToken (): Promise<StopperFn> {
-    log.verbose('OfficialAccount', 'updateAccessToken()')
+  protected startSyncingAccessToken (): StopperFn {
+    log.verbose('OfficialAccount', 'startSyncingAccessToken()')
 
-    try {
-      /**
-       * updated: {
-       *  "access_token":"3...Q",
-       *  "expires_in":7200
-       * }
-       */
-      const ret = await this.simpleUnirest
-        .get<Partial<ErrorPayload> & {
-          access_token : string
-          expires_in   : number
-        }>(`token?grant_type=client_credential&appid=${this.options.appId}&secret=${this.options.appSecret}`)
+    const marginSeconds = 5 * 60  // 5 minutes
+    const tryAgainSeconds = 60    // 1 minute
 
-      log.verbose('OfficialAccount', 'updateAccessToken() updated: %s', JSON.stringify(ret.body))
+    let timer: undefined | NodeJS.Timer
 
-      if (ret.body.errcode && ret.body.errcode > 0) {
-        // {"errcode":40164,"errmsg":"invalid ip 111.199.187.71 ipv6 ::ffff:111.199.187.71, not in whitelist hint: [H.BDtZFFE-Q7bNKA] rid: 5f283869-46321ea1-07d7260c"}
-        throw new Error(`Official Account updateAccessToken() Error ${ret.body.errcode}: ${ret.body.errmsg}`)
-      }
-
-      this.accessTokenPayload = {
-        expiresIn : ret.body.expires_in,
-        timestamp : Date.now(),
-        token     : ret.body.access_token,
-      }
-
-      // 5 minute before expire
-      const MINUTES_BEFORE_EXPIRE = 60 * 5
-      const expireInMs = (ret.body.expires_in - MINUTES_BEFORE_EXPIRE) * 1000
-
-      log.verbose('OfficialAccount', 'updateAccessToken() setInterval() for every %s seconds',
-        Math.floor(expireInMs / 1000),
+    const update = () => this.updateAccessToken()
+      .then(succeed => succeed
+        ? this.accessTokenPayload!.expiresIn - marginSeconds
+        : tryAgainSeconds
       )
+      .then(seconds => setTimeout(update, seconds * 1000))
+      // eslint-disable-next-line no-return-assign
+      .then(newTimer => timer = newTimer)
+      .catch(e => log.error('OfficialAccount', 'startSyncingAccessToken() update() rejection: %s', e))
 
-      const timer = setInterval(async () => {
-        try {
-          log.verbose('OfficialAccount', 'updateAccessToken() setInterval() updating...')
-          await this.updateAccessToken()
-        } catch (e) {
-          log.warn('OfficialAccount', 'updateAccessToken() setInterval() rejection: %s', e)
-
-          log.warn('OfficialAccount', 'updateAccessToken() setInterval() retry after 1 minute')
-          setTimeout(() => this.updateAccessToken(), 60 * 1000)
-        }
-      }, expireInMs)
-
-      return () => clearInterval(timer)
-
-    } catch (e) {
-      log.warn('OfficialAccount', 'updateAccessToken() rejection: %s', e)
-      throw e
+    if (!this.accessTokenPayload) {
+      void update()
+    } else {
+      const seconds = this.accessTokenPayload.expiresIn - marginSeconds
+      timer = setTimeout(update, seconds * 1000)
     }
+
+    return () => timer && clearTimeout(timer)
   }
 
   async sendCustomMessagePersonal (args: {
-    touser: string,
-    msgtype: OAMessageType,
-    content: string,
+    touser  : string,
+    msgtype : OAMessageType,
+    content : string,
   }) {
     this.webhook.emit('instantReply', args)
   }
