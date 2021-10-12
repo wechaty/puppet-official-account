4531e20bcf27976227034316a479b28e130aedf9
zzzyer <72653414+zzzyer@users.noreply.github.com>
2021-10-12 16:41:25 +0800

    Update ding-dong-bot.ts

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -24,7 +24,7 @@ import {
   EventMessagePayload,
   FileBox,
   MessageType,
-  urlLinkPayload,
+  UrlLinkPayload,
 }                         from 'wechaty-puppet'
 
 import { PuppetOA } from '../src/mod'
@@ -125,7 +125,7 @@ async function onMessage (payload: EventMessagePayload) {
     }
   } else if (/link/i.test(msgPayload.text || '')) {
     const imagePath = 'http://mmbiz.qpic.cn/mmbiz_jpg/lOBFkCyo4n9Qhricg66uEO2Ycn9hcCibauvalenRUeMzsRia2VjLok4Gd1iaeuKiarVggr4apCEUNiamIM4FLkpxgurw/0'
-    const wechatyLink: urlLinkPayload = ({ description: 'this is wechaty', thumbnailUrl: imagePath, title: 'WECHATY', url:'https://wechaty.js.org/' })
+    const wechatyLink: UrlLinkPayload = ({ description: 'this is wechaty', thumbnailUrl: imagePath, title: 'WECHATY', url:'https://wechaty.js.org/' })
     await puppet.messageSendUrl(msgPayload.fromId!, wechatyLink,)
   } else if (msgPayload.type === MessageType.Image) {
     const imageFile = FileBox.fromUrl(msgPayload.filename + '.jpg')
